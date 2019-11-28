cdd00d9760b1ed4de94fc8d3488013fa145508fb
王宋军 <wangsongjun@yofish.com>
2019-11-28 14:27:20 +0800

    补充数据，发布配置的集群类型

--- docs/sql/1.5.sql
+++ docs/sql/1.5.sql
@@ -1,5 +1,11 @@
+-- 发布配置添加机器节点字段
 ALTER TABLE `ata_ops`.`pms_app_deploy_config`
   ADD COLUMN `cluster_nodes` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '发布节点' AFTER `start_command`;
 
+-- 发布配置添加集群类型标识
 ALTER TABLE `ata_ops`.`pms_app_deploy_config`
   ADD COLUMN `cluster_type` varchar(1) NULL COMMENT '集群类型: 0:MESOS集群 1:KUBERNETES集群 2:单节点集群' AFTER `env_id`;
+
+-- 补充数据，发布配置的集群类型
+UPDATE pms_app_deploy_config t
+SET t.cluster_type = ( SELECT c.type FROM rms_env e LEFT JOIN rms_cluster c ON e.cluster_id = c.id WHERE e.id = t.env_id );
