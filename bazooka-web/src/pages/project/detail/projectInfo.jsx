e69b0c54a8abfb136033f26559ed74c3a4b8c332
haowenqin <haowenqin@youyuwo.com>
2019-11-29 10:55:12 +0800

    项目资源布局

--- bazooka-web/src/pages/project/detail/projectInfo.jsx
+++ bazooka-web/src/pages/project/detail/projectInfo.jsx
@@ -363,11 +363,13 @@ class ProjectInfo extends React.Component {
                     ]
                   })(
                     <Checkbox.Group style={{ width: '100%', lineHeight: 'unset' }}>
-                      <Row type="flex" align="middle">
+                      <Row type="flex">
                         {
                           envList.map((item, i) => (
-                            <Col span={11} key={item.id}>
-                              <Checkbox value={item.id} disabled={selectedEnvIds.indexOf(item.id) > -1 ? true : false}>{item.name}</Checkbox>
+                            <Col span={12} key={item.id} title={item.name}>
+                              <Checkbox value={item.id} disabled={selectedEnvIds.indexOf(item.id) > -1 ? true : false} className={styles.textOverflow} style={{width: '100%'}} >
+                                {item.name}
+                              </Checkbox>
                             </Col>
                           ))
                         }
